886ee0a0805e9bcdb86f9aa1e2f00069ff4a7a98
vinhtd <taduyvinh@gmail.com>
2016-10-11 00:18:17 +0700

    The User must be able to go two pages: Menu, and Contact Us.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def home
+  	
+  end
+
+  def contact_us
+
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'menu' => 'home#menu' 
+  get 'contact_us' => 'home#contact_us'
+
+	root 'home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
